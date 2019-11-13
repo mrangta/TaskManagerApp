a4d4f1ea6cfbfca08f97e0ed31b8fb487c19cf33
Worek Dawid <dawid.worek@aalto.fi>
2019-11-13 10:33:13 +0200

    Updating attachments in database, start working on image resizing

--- frontend/app/src/main/java/com/mcc/g22/AttachmentsManager.kt
+++ frontend/app/src/main/java/com/mcc/g22/AttachmentsManager.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -15,7 +16,22 @@ import java.io.File
  * @param projectId ID of the project to work on
  */
 class AttachmentsManager(private var projectId: String) {
-    private var storage: FirebaseStorage = FirebaseStorage.getInstance()
+    private val storage: FirebaseStorage = FirebaseStorage.getInstance()
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+    init {
+        // Enable persistence to be sure that realtime database will be updated
+        database.setPersistenceEnabled(true)
+    }
+
+    /**
+     * This class lists all possible settings of image size
+     */
+    enum class ImageSize(val size: String) {
+        LOW("_640x480"),
+        HIGH("_1280x960"),
+        FULL(""),
+    }
 
     /**
      * Upload new attachment to the project. Register attachment in configuration of the project.
@@ -25,13 +41,16 @@ class AttachmentsManager(private var projectId: String) {
      * @param onFailure function to be called when the file could not be uploaded
      * @param fileName change name of the file in the storage to this name. If empty, filename is
      *                  not changed
+     * @param imageSize If file to send is an image, it will be resized to the given size before
+     *                  uploading it to the storage.
+     *                  If file is not an image, this parameter ignored
      */
     fun uploadFile(
         uri: Uri, onFileUploaded: () -> Unit, onFailure: () -> Unit,
-        fileName: String = ""
+        fileName: String = "", imageSize: ImageSize = ImageSize.FULL
     ) {
 
-        var filenameInStorage = uri.lastPathSegment
+        var filenameInStorage: String = uri.lastPathSegment.toString()
         if (fileName != "") filenameInStorage = fileName
 
         val newFile = storage.reference.child("$projectId/$filenameInStorage")
@@ -40,7 +59,13 @@ class AttachmentsManager(private var projectId: String) {
         uploadTask.addOnFailureListener {
             onFailure()
         }.addOnSuccessListener {
-            // TODO register uploaded file in the Realtime Database
+
+            // Register uploaded file in the Realtime Database
+            database.reference.child("projects")
+                .child(projectId)
+                .child("attachments")
+                .child(filenameInStorage).setValue(true)
+
             onFileUploaded()
         }
     }
@@ -52,10 +77,13 @@ class AttachmentsManager(private var projectId: String) {
      * @param onFileUploaded function to be called when the file is uploaded successfully
      * @param onFailure function to be called when the file could not be uploaded
      * @param fileName name of the file
+     * @param imageSize If file to send is an image, it will be resized to the given size before
+     *                  uploading it to the storage.
+     *                  If file is not an image, this parameter ignored
      */
     fun uploadFile(
         bitmap: Bitmap, onFileUploaded: () -> Unit, onFailure: () -> Unit,
-        fileName: String
+        fileName: String, imageSize: ImageSize = ImageSize.FULL
     ) {
 
         val newFile = storage.reference.child("$projectId/$fileName")
@@ -67,7 +95,13 @@ class AttachmentsManager(private var projectId: String) {
         uploadTask.addOnFailureListener {
             onFailure()
         }.addOnSuccessListener {
-            // TODO register uploaded file in the Realtime Database
+
+            // Register uploaded file in the Realtime Database
+            database.reference.child("projects")
+                .child(projectId)
+                .child("attachments")
+                .child(fileName).setValue(true)
+
             onFileUploaded()
         }
     }
@@ -80,12 +114,15 @@ class AttachmentsManager(private var projectId: String) {
      * @param onFailure function to be called when the file could not be uploaded
      * @param fileName change name of the file in the storage to this name. If empty, filename is
      *                  not changed
+     * @param imageSize If file to send is an image, it will be resized to the given size before
+     *                  uploading it to the storage.
+     *                  If file is not an image, this parameter ignored
      */
     fun uploadFile(
         file: File, onFileUploaded: () -> Unit, onFailure: () -> Unit,
-        fileName: String = ""
+        fileName: String = "", imageSize: ImageSize = ImageSize.FULL
     ) {
-        uploadFile(Uri.fromFile(file), onFileUploaded, onFailure, fileName)
+        uploadFile(Uri.fromFile(file), onFileUploaded, onFailure, fileName, imageSize)
     }
 
     /**
@@ -96,10 +133,13 @@ class AttachmentsManager(private var projectId: String) {
      * @param onFailure function to be called when file cannot be downloaded
      * @param localFile local file where downloaded file should be saved
      *                  By default, temporary file is created (with name defined in @see fileName)
+     * @param imageSize If file to download is an image, it will be downloaded in the given size.
+     *                  If image is not available in the given size, the closest size
+     *                  to the requested is downloaded
      */
     fun downloadFile(
         fileName: String, onFileDownloaded: (downloadedFile: File) -> Unit,
-        onFailure: () -> Unit, localFile: File? = null
+        onFailure: () -> Unit, localFile: File? = null, imageSize: ImageSize = ImageSize.FULL
     ) {
 
         var dstFile = localFile
@@ -124,8 +164,12 @@ class AttachmentsManager(private var projectId: String) {
      * @param fileName name of the image to load
      * @param context application context
      * @param imageView view to show the image
+     * @param imageSize If file to download is an image, it will be downloaded in the given size.
+     *                  If image is not available in the given size, the closest size
+     *                  to the requested is downloaded
      */
-    fun loadImage(fileName: String, context: Context, imageView: ImageView) {
+    fun loadImage(fileName: String, context: Context, imageView: ImageView,
+                  imageSize: ImageSize = ImageSize.FULL) {
 
         val imageRef = storage.reference.child("$projectId/$fileName")
         Glide.with(context).load(imageRef).into(imageView)
