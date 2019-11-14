6e863403dc62272101d73a4addde8d792d6301b3
Parinaz Avaznejad <parinaz.avaznejad@aalto.fi>
2019-11-15 00:08:08 +0200

    login activity

--- frontend/app/src/main/java/com/mcc/g22/LoginActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/LoginActivity.kt
@@ -1,2 +1,24 @@
 package com.mcc.g22
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var buttonRegister :Button
+    private lateinit var loginAuth : FirebaseAuth
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        buttonRegister.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+
+        }
+    }
+}
--- frontend/app/src/main/java/com/mcc/g22/RegisterActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/RegisterActivity.kt
@@ -1,2 +1,24 @@
 package com.mcc.g22
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var buttonLogin: Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        buttonLogin.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+
+        }
+    }
+}
