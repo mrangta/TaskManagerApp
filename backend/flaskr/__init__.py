77d88b5d1242c00153cbe24ae05ac1d459b32e67
Aleksi Vuorjoki <aleksi.vuorjoki@avarko.com>
2019-11-15 00:54:53 +0200

    openapi spec updated and a mock api implemented

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,18 +1,38 @@
-import os
-
-from flask import Flask
-
+from flask import Flask, jsonify, make_response, current_app
 
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-   # app.config.from_mapping(
-    #    SECRET_KEY='dev',
-   # )
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hellow!'
+    @app.route('/projects/<project_id>/tasks', methods=['POST'])
+    def tasks(project_id):
+        return {'id': 'task_id'}, 201
+
+    @app.route('/projects/<project_id>/tasks/<task_id>', methods=['PUT'])
+    def taskWithId(project_id, task_id):
+        return jsonify_no_content()
+
+    @app.route('/projects/<project_id>/tasks/<task_id>/users', methods=['PUT'])
+    def taskUsers(project_id, task_id):
+        return jsonify_no_content()
+
+    @app.route('/projects', methods=['POST'])
+    def projects(project_id):
+        return {'id': 'project_id'}, 201
+
+    @app.route('/projects/<project_id>', methods=['DELETE'])
+    def projectWithId(project_id):
+        return jsonify_no_content()
+
+    @app.route('/projects/<project_id>/members', methods=['PUT'])
+    def projectMembers(project_id):
+        return jsonify_no_content()
+
 
     return app
+
+def jsonify_no_content():
+    response = make_response('', 204)
+    response.mimetype = current_app.config['JSONIFY_MIMETYPE']
+
+    return response
