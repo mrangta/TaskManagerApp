9210b019ab25bd46287ebec50b4975b7fc7ccea4
Worek Dawid <dawid.worek@aalto.fi>
2019-12-01 15:37:18 +0200

    Searching users in the database

--- frontend/app/src/main/java/com/mcc/g22/User.kt
+++ frontend/app/src/main/java/com/mcc/g22/User.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 
 class User(val username: String) {
@@ -16,13 +19,43 @@ class User(val username: String) {
 
     companion object {
         private val storage: FirebaseStorage = FirebaseStorage.getInstance()
-        private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        private val database = FirebaseDatabase.getInstance()
+            .reference.child("users")
 
         /**
          * Return user registered on this device
          */
         fun getRegisteredUser(): User? {
             return User("test user")
+        }
+
+        /**
+         * Find users with display names which start with usernameStart
+         *
+         * @param usernameStart the beginning of the username to find
+         */
+        fun searchForUsers(usernameStart: String,
+                           onUsersFound: (fullUsernames: Set<String>) -> Unit,
+                           onFailure: () -> Unit,
+                           limitNumberOfResults: Int = 50) {
+            val usersRef = database.orderByKey().startAt(usernameStart)
+                                .endAt(usernameStart + "\uf8ff")
+                                .limitToFirst(limitNumberOfResults)
+                                .ref
+
+            usersRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(databaseError: DatabaseError) {
+                    onFailure()
+                }
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val usersNames = mutableSetOf<String>()
+                    for (usr in dataSnapshot.children) {
+                        usersNames.add( usr.key as String )
+                    }
+                    onUsersFound(usersNames)
+                }
+            })
         }
     }
 
@@ -34,8 +67,7 @@ class User(val username: String) {
         uploading.addOnCanceledListener { onFailure() }
         uploading.addOnCompleteListener {
             profileImage = tmpImage
-            database.reference.child("users").child(username).child("profile_image")
-                .setValue(profileImage)
+            database.child(username).child("profile_image").setValue(profileImage)
             onProfileImageUploaded()
         }
     }
