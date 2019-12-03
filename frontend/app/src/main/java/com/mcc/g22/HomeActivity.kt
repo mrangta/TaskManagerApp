cd6d007883f392aecac502bb4a31d5f55dcf5d0b
Parinaz Avaznejad <parinaz.avaznejad@aalto.fi>
2019-12-03 12:15:24 +0200

    create homeActivity with navigation

--- frontend/app/src/main/java/com/mcc/g22/HomeActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/HomeActivity.kt
@@ -2,11 +2,33 @@ package com.mcc.g22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(home_toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        NavigationUI.setupWithNavController(nav_menu_view , navController)
+        NavigationUI.setupActionBarWithNavController(this , navController , drawer_layout)
+
+
     }
+
+    //opening our navigation drawer and also back button
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this , R.id.nav_host_fragment), drawer_layout)
+    }
+    
 }
