d631642e5d4ccc34613470c8e87058021bbe886e
Yizhou Ye <yizhou.ye@aalto.fi>
2019-12-03 19:08:03 +0200

    Add a separate change_password screen

--- frontend/app/src/main/java/com/mcc/g22/ChangePasswordActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/ChangePasswordActivity.kt
@@ -1,31 +1,22 @@
 package com.mcc.g22
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.view.GravityCompat.*
-import kotlinx.android.synthetic.main.activity_dashboard.*
 import android.view.MenuItem
+import android.view.View
+import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
+class ChangePasswordActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
 
-class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_change_password)
         nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    fun toggleDrawer(view: View){
-        if(drawer_layout.isDrawerOpen(START)) {
-            drawer_layout.closeDrawer(START)
-        }
-        else {
-            drawer_layout.openDrawer(START)
-        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -62,7 +53,21 @@ class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
 
     }
 
-   
+    fun toggleDrawer(view: View){
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
 
+    fun changePassword(view: View) {
 
+    }
+
+    fun returnHome(view: View) {
+        intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+    }
 }
--- frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
@@ -3,17 +3,55 @@ package com.mcc.g22
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.View.VISIBLE
 import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_dashboard.drawer_layout
 import kotlinx.android.synthetic.main.activity_edit_profile.*
+import kotlinx.android.synthetic.main.activity_edit_profile.nav_view
 
-class EditProfileActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.getItemId()) {
+            R.id.nav_profile -> {
+                showProfile()
+                return true
+            }
+            R.id.nav_changePassword -> {
+                changePassword()
+                return true
+            }
+            R.id.nav_logOut -> {
+                logOut()
+                return true
+            }
+
+        }
+        return true
+    }
+
+    fun showProfile() {
+        intent = Intent(this, EditProfileActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun changePassword() {
+        intent = Intent(this, ChangePasswordActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun logOut() {
+
     }
 
     fun toggleDrawer(view: View){
@@ -23,17 +61,6 @@ class EditProfileActivity : AppCompatActivity() {
         else {
             drawer_layout.openDrawer(GravityCompat.START)
         }
-    }
-
-    fun changePassword(view: View) {
-        currentPassword.visibility = VISIBLE
-        newPassword.visibility = VISIBLE
-        confirmNewPassword.visibility = VISIBLE
-        update.visibility = VISIBLE
-    }
-
-    fun updatePassword(view: View) {
-
     }
 
     fun returnHome(view: View) {
