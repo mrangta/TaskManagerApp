dd23dd5c54ecd7dca1dab04186486035aee07c31
Yizhou Ye <yizhou.ye@aalto.fi>
2019-12-03 13:48:19 +0200

    Add onClick functionality to navigation items

--- frontend/app/src/main/java/com/mcc/g22/DashboardActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/DashboardActivity.kt
@@ -1,18 +1,22 @@
 package com.mcc.g22
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.GravityCompat.*
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import android.view.MenuItem
+import com.google.android.material.navigation.NavigationView
 
-class DashboardActivity : AppCompatActivity() {
+
+class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     fun toggleDrawer(view: View){
@@ -23,6 +27,48 @@ class DashboardActivity : AppCompatActivity() {
             drawer_layout.openDrawer(START)
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.getItemId()) {
+            R.id.nav_profile -> {
+                showProfile()
+                return true
+            }
+            R.id.nav_notifications -> {
+                showNotifications()
+                return true
+            }
+            R.id.nav_projects -> {
+                showCreatedProjects()
+                return true
+            }
+            R.id.nav_logOut -> {
+                logOut()
+                return true
+            }
+
+        }
+        return true
+    }
+
+    fun showProfile() {
+        intent = Intent(this, EditProfileActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun showNotifications() {
+
+    }
+
+    fun showCreatedProjects() {
+
+    }
+
+    fun logOut() {
+
+    }
+
+   
 
 
 }
---
+++ frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.mcc.g22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
