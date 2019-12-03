3b4cbab157bbfa1e2e9d4714408892bb5ec78f28
Yizhou Ye <yizhou.ye@aalto.fi>
2019-12-03 15:48:16 +0200

    Add border radius on edit_text

--- frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
@@ -2,11 +2,23 @@ package com.mcc.g22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class EditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+    }
+
+    fun toggleDrawer(view: View){
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
     }
 }
