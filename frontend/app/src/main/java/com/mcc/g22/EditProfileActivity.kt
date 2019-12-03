c3dd4e133fd6cc04d3c16f9dc3a91e0857d9cf35
Yizhou Ye <yizhou.ye@aalto.fi>
2019-12-03 18:39:22 +0200

    Add svg support and edit_profile screen

--- frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
+++ frontend/app/src/main/java/com/mcc/g22/EditProfileActivity.kt
@@ -1,10 +1,13 @@
 package com.mcc.g22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.VISIBLE
 import androidx.core.view.GravityCompat
-import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_dashboard.drawer_layout
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -20,5 +23,21 @@ class EditProfileActivity : AppCompatActivity() {
         else {
             drawer_layout.openDrawer(GravityCompat.START)
         }
+    }
+
+    fun changePassword(view: View) {
+        currentPassword.visibility = VISIBLE
+        newPassword.visibility = VISIBLE
+        confirmNewPassword.visibility = VISIBLE
+        update.visibility = VISIBLE
+    }
+
+    fun updatePassword(view: View) {
+
+    }
+
+    fun returnHome(view: View) {
+        intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
     }
 }
