09f3aca70035ac5e777dcd21a0b24a091b7654ec
Aleksi Vuorjoki <aleksi.vuorjoki@avarko.com>
2019-12-04 16:44:01 +0200

    moved api to a versioned path /v1/

--- backend/conf/gunicorn_config.py
+++ backend/conf/gunicorn_config.py
@@ -4,6 +4,5 @@ bind = "0.0.0.0:{}".format(os.environ.get("PORT"))
 reload = False
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-errorlog = 'error.log'
 if(os.environ.get("ENV") == 'dev'):
     reload = True
