eab9585c774395bc938b5a8ef5c78fa7bec8f082
Monika Rangta <monika.rangta@aalto.fi>
2019-12-07 19:45:00 +0200

    Updated UI of Project tasks, project files and project pictures

--- frontend/app/src/main/java/com/mcc/g22/ProjectListAdapter.kt
+++ frontend/app/src/main/java/com/mcc/g22/ProjectListAdapter.kt
@@ -25,6 +25,7 @@ class ProjectListAdapter(private val mDataList: ArrayList<ProjectListDetails>, v
         holder.ptitle.text = mDataList[position].project_title
         holder.itemView.findViewById<View>(R.id.project_tasks).setOnClickListener { clickListener(item, position) }
         holder.itemView.findViewById<View>(R.id.project_title).setOnClickListener { clickListener(item, position) }
+        holder.itemView.findViewById<View>(R.id.profile_picture).setOnClickListener { clickListener(item, position) }
     }
 
     override fun getItemCount(): Int {
